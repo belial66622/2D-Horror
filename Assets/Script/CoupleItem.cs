c2e92ca90fca6430e8b8e34b996732adb8243adf
Bill Panjaitan <killbill.panjaitan@gmail./com>
2024-07-21 20:27:39 +0700

    stage 2

--- Assets/Script/CoupleItem.cs
+++ Assets/Script/CoupleItem.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using static UnityEditor.ShaderData;
 
-public class CoupleItem : MonoBehaviour
+public class CoupleItem : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] CoupleItem Other;
+
+    public bool HaveItem = false;
+
+    public UnityEvent OnOtherHaveItem;
+
+    [SerializeField] private string keyItem;
+    public void CancelInteraction()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void interaction(object obj = null)
     {
-        
+        if (HaveItem)
+        {
+            EventContainer.Instance.DialogueEvent($"Item Sudah diberikan");
+        }
+
+        else
+        {
+            Transform character = (Transform)obj;
+            if (KeyItem(character) == keyItem)
+            {
+                EventContainer.Instance.DialogueEvent($"Artifak Ditaruh Disini");
+                HaveItem = true;
+                if (Other.HaveItem)
+                {
+                    OnOtherHaveItem?.Invoke();
+                    EventContainer.Instance.DialogueEvent($"Pintu Sudah Terbuka");
+                }
+            }
+            else
+            {
+                EventContainer.Instance.DialogueEvent($"Seharusnya disini tempat artifak");
+            }
+        }
+    }
+
+    private string KeyItem(Transform player)
+    {
+        string item;
+        if (player.TryGetComponent<Inventory>(out Inventory inventory))
+        {
+            item = inventory.Choosenitem();
+
+            return item;
+        }
+
+        return "";
     }
 }
